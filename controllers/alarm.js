e77038ad58bbbeb17c0b4e637330de1796269b7e
GDmail <GDmail91@gmail.com>
2016-07-23 22:55:14 +0900

    Add alarm check

--- controllers/alarm.js
+++ controllers/alarm.js
@@ -16,8 +16,9 @@ router.get('/', function(req, res, next) {
 
     async.waterfall([
         function(callback) {
-            require('../models/alarm_model').setCheck(data, function (status, msg) {
-                if (status) callback(null);
+            // TODO 게시물 리스트 가져옴
+            require('../models/alarm_model').getAlarm(data, function(status, msg, data) {
+                if (status) callback(null, data);
                 else callback(msg);
             });
         }
