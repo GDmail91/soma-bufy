47d880029359fde104c150c316efa26787d20441
GDmail <GDmail91@gmail.com>
2016-07-23 22:58:26 +0900

    Add alarm check

--- controllers/ranking.js
+++ controllers/ranking.js
@@ -338,7 +338,7 @@ router.get('/:content_id', function(req, res, next) {
         function(callback) {
             if(typeof data.is_check == 'undefined') return callback(null);
             else {
-                require('../models/alarm_model').setAlarm(data, function(status, msg) {
+                require('../models/alarm_model').setCheck(data, function(status, msg) {
                     if (status) callback(null);
                     else callback(msg);
                 });
